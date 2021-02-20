eaf321c9438aef0549f92ff109525f5055c79e73
Ivan <34632454+abreulima@users.noreply.github.com>
2021-02-20 12:01:05 -0400

    updates

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,12 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
+url = "https://abreulima.github.io/web-scrapping/"
+
+source = requests.get(url).content
+
+soup = BeautifulSoup(source, features="html.parser")
+
+main_title = soup.find(id="main-title")
+
+print(main_title)
